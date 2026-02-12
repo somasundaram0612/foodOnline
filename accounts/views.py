9013f243aec6f23cea393fd71fa6268cf5dab300
somasundaram0612 <somasundaram822@gmail.com>
2026-02-12 23:33:48 +0530

    vendor registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from .models import User
+from .models import User, UserProfile
 from .forms import UserForm
 
 from django.contrib import messages
+
+from vendor.models import Vendor
+from vendor.forms import VendorForm
 
 # Create your views here.
 def registerUser(request):
@@ -50,4 +53,51 @@ def registerUser(request):
         'form':form,
     }
     return render(request, 'accounts/registerUser.html', context)
+
+
+
+def registerVendor(request):
+    if request.method == 'POST':
+        # store the data and create the user
+        form = UserForm(request.POST)
+        v_form = VendorForm(request.POST, request.FILES)
+        if form.is_valid() and v_form.is_valid():
+            
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(
+               first_name=first_name,
+               last_name=last_name,
+               username=username,
+               email=email,
+               password=password
+            )
+            user.role = User.VENDOR
+            user.save()
+            
+            vendor = v_form.save(commit=False)
+            vendor.user = user
+            user_profile = UserProfile.objects.get(user=user)
+            vendor.user_profile = user_profile
+            vendor.save()
+
+            # print('User is created')
+            messages.success(request, 'Your account has been registered successfully! Please wait for the approval..')             
+            return redirect('registerVendor')
+        
+        else:
+            print('invalid form')
+            print(form.errors)
+    else:
+        form = UserForm()
+        v_form = VendorForm()
+
+    context = {
+        'form': form,
+        'v_form': v_form,
+    }
+    return render(request, 'accounts/registerVendor.html', context)
 
